a837137bdb6293c13d6e2f70a420243fa3527795
Marina-Saladukha <marina@reunionmarketing.com>
2018-02-25 00:54:10 -0500

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,31 +11,38 @@
     var trivia = [{
      question: "1. Which of the following is NOT one of the seven wonders of the world?",
      choices: ["Michelangelo’s David","Taj Mahal", "Great Wall of China", "Lighthouse at Alexandria"],
-     correctAnswer: "Michelangelo’s David"
+     correctAnswer: "Michelangelo’s David",
+     giphy: "https://www.visionaireworld.com/blog/wp-content/uploads/2016/07/David.gif"
    },{
      question: "2. Which mammal has the most teeth?",
      choices: ["Dolphin","Opossum", "Northern Elephant Seal", "Hippopotamus"],
-     correctAnswer: "Dolphin"
+     correctAnswer: "Dolphin",
+     giphy: "https://media.giphy.com/media/12BJgUnSfDTcFa/giphy.gif"
    },{
      question: "3. The idea of periodic table came to Dmitry Mendeleev:",
      choices: ["From his misstress","In his sleep", "During an argument", "After a head trauma"],
-     correctAnswer: "In his sleep"
+     correctAnswer: "In his sleep",
+     giphy: "https://media.giphy.com/media/ZOg4aH7msZs52/giphy.gif"
    },{
      question: "4. What was the first country that allowed women to vote?",
      choices: ["France","USA", "New Zealand", "Saudi Arabia"],
-     correctAnswer: "New Zealand"
+     correctAnswer: "New Zealand",
+     giphy: "https://thumbs.gfycat.com/AngelicMelodicDegus-max-1mb.gif"
    },{
      question: "5. What is the most recycled material in the US?",
      choices: ["Plastic","Asphalt", "Aluminum", "Steel"],
-     correctAnswer: "Asphalt"
+     correctAnswer: "Asphalt",
+     giphy: "https://media1.giphy.com/media/iGU33YlzPxY8o/giphy.gif"
    }, {
      question: "6. How much of your total body weight are bones?",
      choices: ["15%", "6%", "32%", "47%"],
-     correctAnswer: "15%"
+     correctAnswer: "15%",
+     giphy: "http://cdn.fishki.net/upload/post/201511/04/1723462/010.gif"
    },{
      question: "7. What was the first car company in the world?",
      choices: ["Peugeot","Fiat", "Mercedes-Benz", "Ford"],
-     correctAnswer: "Mercedes-Benz"
+     correctAnswer: "Mercedes-Benz",
+     giphy: "https://thumbs.gfycat.com/SneakyImpishGaur-size_restricted.gif"
    }];
 
    var wins = 0;
@@ -57,7 +64,6 @@
     function clock() {
       $(".timer").html("Time Remaining: <br>" + seconds);
       seconds--;
-
       if (seconds < 0 ) {
         clearInterval(intervalId);
         losses++;
@@ -70,8 +76,9 @@
 
   //displaying question
   function displayQuestion(qId) {
-      
-      if (qId > trivia.length) {
+      var seconds = 5;
+      $(".timer").show();
+      if (qId >= trivia.length) {
         $("#theGame").html("<p>Game Over!!</p>");
         $(".wins").html("<p>Wins:" + wins + "</p>");
         $(".losses").html("<p>Losses:" + losses + "</p>");
@@ -84,31 +91,26 @@
     }
 
 
- // function answerPick() {   
-
     $(document).on("click", "li", function(e) {
-        console.log(clicked)
-        // if (clicked) return clicked = false;
+       $(".timer").hide();
        var correctAnswer = trivia[count].correctAnswer;
        var chosenAnswer = $(this).text();
+       var g = trivia[count].giphy;
        if (correctAnswer === chosenAnswer) {
+         $("#theGame").html("<div class='question'>Great Job! The correct answer is <span class='correct-answer'>" + correctAnswer + "</span></div><iframe src='" + g + "'width='480' height='474' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>");
          clearInterval(intervalId);
-         alert("Great Job!");
          wins++;
-         count++;
-         seconds = 5;
+         seconds = 4;
          intervalId = setInterval(clock, 1000);
-         displayQuestion(count);
        }
         else if (correctAnswer !== chosenAnswer) {
-         alert("No, the correct answer is " + correctAnswer + "!");
+         $("#theGame").html("<div class='question'>No, the correct answer is " + correctAnswer + "</div><iframe src='" + g + "'width='480' height='474' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>");
          clearInterval(intervalId);
          losses++;
-         count++;
-         seconds = 5;
+         seconds = 4;
          intervalId = setInterval(clock, 1000);
-         displayQuestion(count);
       }
+
 
   });
     //}
